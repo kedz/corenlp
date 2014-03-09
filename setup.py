8d29ff03f703c1a43ae5a09c2a7f3a3c73a38d97
kedz <crk2130@columbia.edu>
2014-03-09 00:48:06 -0500

    Submitted new version to pip.

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name = 'corenlp',
     packages = ['corenlp'],
-    version = '0.0.3',
+    version = '0.0.6',
     description = 'A python wrapper for the Stanford CoreNLP java library.',
     author='Chris Kedzie',
     author_email='kedzie@cs.columbia.edu',
-    url='www.github.com/kedz'
-    )
+    url='www.github.com/kedz/corenlp',
+    install_requires=[
+        'nltk',
+    ]
+
+)
                     
