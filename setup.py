415774c6ec792a4bf4175728db03d89c1fab6f73
kedz <crk2130@columbia.edu>
2014-03-11 16:46:06 -0400

    setup.py github and pypi now agree on current version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'corenlp',
     packages = ['corenlp'],
-    version = '0.0.6',
+    version = '0.0.7',
     description = 'A python wrapper for the Stanford CoreNLP java library.',
     author='Chris Kedzie',
     author_email='kedzie@cs.columbia.edu',
