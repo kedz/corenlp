7d32dfe338ae143777f23dd564d0f87ac3f2caa0
kedz <kedzie@cs.columbia.edu>
2014-04-10 17:01:49 -0400

    Updated setup version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'corenlp',
     packages = ['corenlp'],
-    version = '0.0.11',
+    version = '0.0.12',
     description = 'A python wrapper for the Stanford CoreNLP java library.',
     author='Chris Kedzie',
     author_email='kedzie@cs.columbia.edu',
