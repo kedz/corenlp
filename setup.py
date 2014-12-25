0791259570a8e7e014ae1bf2b4e2a926bbe014dc
kedz <kedzie@cs.columbia.edu>
2014-12-24 21:16:16 -0500

    Build server code and check for dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,52 @@
 from setuptools import setup
+import os
+import imp
+import sys
+import shutil
+
+def validate_deps():
+    path = os.getenv(u'CORENLP_DIR', None)
+    if path is None:
+        print u'Set CORENLP_DIR environment variable to the CoreNLP directory.'
+        sys.exit(1) 
+
+    ver = os.getenv(u'CORENLP_VER', None)
+    if ver is None:
+        print u'Set CORENLP_VER environment variable to the version of CoreNLP.'
+        sys.exit(1) 
+
+    return path, ver
+
+def build_server(corenlp_path, corenlp_ver):
+    path = os.path.dirname(os.path.realpath(__file__))
+    file, pathname, description = imp.find_module('util', [os.path.join(path, 'corenlp')])
+    util = imp.load_module('util', file, pathname, description)
+    file.close()
+    classpath = util.validate_jars(corenlp_path, corenlp_ver)
+    src = os.path.join(path, u"server-src", u"*.java")
+    server_src = os.path.join(path, u"server-src", u"CoreNlpServer.java")
+    handler_src = os.path.join(path, u"server-src", u"CoreNlpHandler.java")
+    server_cla = os.path.join(path, u"server-src", u"CoreNlpServer.class")
+    handler_cla = os.path.join(path, u"server-src", u"CoreNlpHandler.class")
+    os.system('javac -cp {} {}'.format(classpath, src))
+    if not os.path.exists(server_src) or not os.path.exists(handler_src):
+        print "Failed to build java server."
+        sys.exit(1)
+    else: 
+        bin_dir = os.path.join(path, u'corenlp', u'bin')
+        server_dest = os.path.join(bin_dir, u'CoreNlpServer.class')
+        handler_dest = os.path.join(bin_dir, u'CoreNlpHandler.class')
+        if not os.path.exists(bin_dir):
+            os.makedirs(bin_dir)
+        if os.path.exists(server_dest):
+            os.remove(server_dest)
+        if os.path.exists(handler_dest):
+            os.remove(handler_dest)
+        shutil.move(server_cla, bin_dir)
+        shutil.move(handler_cla, bin_dir)
+
+corenlp_path, corenlp_ver = validate_deps()
+build_server(corenlp_path, corenlp_ver)
 
 setup(
     name = 'corenlp',
@@ -10,7 +58,11 @@ setup(
     url='https://github.com/kedz/corenlp',
     install_requires=[
         'nltk',
-    ]
+    ],
+    include_package_data=True,
+    package_data={
+        'corenlp': [os.path.join('bin', 'CoreNlpServer.class'),
+                    os.path.join('bin', 'CoreNlpHandler.class'),]},
 
 )
                     
