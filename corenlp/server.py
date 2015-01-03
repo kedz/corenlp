ba4b8dafbde427aa35d8b37b7abb6c09d0eef24a
Chris Kedzie <kedzie@cs.columbia.edu>
2015-01-03 14:05:07 -0500

    annotate handles timeouts better now

--- corenlp/server.py
+++ corenlp/server.py
@@ -18,33 +18,42 @@ BUFFER_SIZE = 1024
 
 class CoreNLPClient:
 
-    def __init__(self, port=8090):
+    def __init__(self, port=10999):
         self.host = u'localhost'
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.settimeout(15)
         self._num_retries = 3
 
-        try:
-            self.sock.connect((self.host, self.port))
-            self.sock.sendall(CHECK_IN)
-            data = self.sock.recv(BUFFER_SIZE)
-            assert data == SUCCESS
-            self.sock.close()
-        except socket.error, e:    
-            if u'[Errno 111] Connection refused' == unicode(e):
-                import sys
-                err = sys.stderr
-                err.write(u'Could not find CoreNLP server on {}:{}\n'.format(
-                    self.host, self.port))
-                err.flush()
-                sys.exit()
-
+        connected = False
+        n_try = 0
+        while not connected and n_try < self._num_retries:
+            try:
+                self.sock.connect((self.host, self.port))
+                self.sock.sendall(CHECK_IN)
+                data = self.sock.recv(BUFFER_SIZE)
+                assert data == SUCCESS
+                self.sock.close()
+                connected = True
+            except socket.error, e:    
+                if u'[Errno 111] Connection refused' == unicode(e):
+                    import sys
+                    err = sys.stderr
+                    err.write(u'Could not find CoreNLP server on {}:{}\n'.format(
+                        self.host, self.port))
+                    err.flush()
+                    sys.exit()
+                elif '[Errno 99] Cannot assign requested address' in str(e):
+                    print e
+                    print "sleeping for 3 seconds..."
+                    time.sleep(3)
+            n_try += 1
+             
+                
     def annotate(self, text, xml=False):
 
         if isinstance(text, unicode):
             text = text.encode('utf-8')
-        
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
         connected = False
         for i in range(self._num_retries):
@@ -55,7 +64,9 @@ class CoreNLPClient:
                 connected = True
                 break
             except socket.error, e:
-                print "try:", i, str(e)
+                if i > 2:
+                    print "try:", i, str(e)
+                time.sleep(5)
         if not connected:
             raise Exception("Failed to connect to server.")
 
@@ -82,7 +93,7 @@ class CoreNLPClient:
         else:
             return read_xml(StringIO(buf))
 
-def start(port=8090, mem=u'3G', annotators=None, threads=1,
+def start(port=10999, mem=u'3G', annotators=None, threads=1,
           server_start_timeout=60):
     
     cnlp_dir = os.environ.get('CORENLP_DIR')
@@ -135,7 +146,7 @@ def start(port=8090, mem=u'3G', annotators=None, threads=1,
         time.sleep(1)
         duration = time.time() - start
 
-def stop(port=8090):
+def stop(port=10999):
     host = u'localhost'
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -150,7 +161,7 @@ def stop(port=8090):
             raise Exception(u'Could not find CoreNLP server on {}:{}\n'.format(
                 host, port))
 
-def check_status(port=8090):
+def check_status(port=10999):
     server_on = False
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
